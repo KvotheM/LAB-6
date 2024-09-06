b655d98c2c78a63a69956a981439358dda3e4c4a
KvotheM <147577572+KvotheM@users.noreply.github.com>
2024-09-06 17:25:36 -0600

    Update main.cpp, comentarios

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
+/*
+Laboratorio 6 Nicolás Moklebust
+El programa toma el valor de voltaje de dos potenciómetros y los muestra haciendo uso de una LCD, además muestra un contador de 8 bits que aumenta o disminuye
+dependiendo del botón precionado. 
+*/
+
+//Se incluyen las librerías necesarias para el proyect
 #include <Arduino.h>
 #include <LiquidCrystal_I2C.h>
 
+//se definen los pines que se usarán para los potenciómetros, botones y para la I2C
 #define Pot1 15
 #define Pot2 34
 #define Bot1 18
@@ -10,14 +18,14 @@
 #define I2C_SCL 26
 
 
+//Se coloca el prototipo de las interrupciones que se activan al precionar el botón y se define el mux
 void IRAM_ATTR suma(void);
 void IRAM_ATTR resta(void);
 
 portMUX_TYPE mux = portMUX_INITIALIZER_UNLOCKED;
 volatile unsigned long debounce1 = 0;
 
-uint8_t un, decenas, decimal;
-
+//se definen las variables que se usarán para el proyecto y los valores iniciales que estas tendrán para evitar errores en el funcionamiento
 float volt1 = 0.0;
 float volt2 = 0.0;
 int dig1[3] = {0, 0, 0};
@@ -26,18 +34,20 @@ int cambio = 0;
 int envio = 0;
 int contador = 0;
 
+//esecificamos los datos del display
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 
 void setup(){
+//iniciamos el monitor serial
   Serial.begin(115200);
-
+//definimos el modo de funconamiento de los pines
   pinMode(Bot1, INPUT_PULLDOWN);
   pinMode(Bot2, INPUT_PULLDOWN);
-
+//anexamos la interrupción a los pines de los botones
   attachInterrupt(digitalPinToInterrupt(Bot1), suma, RISING);
   attachInterrupt(digitalPinToInterrupt(Bot2), resta, RISING);
-
+//inicializamos la I2C
   lcd.init(I2C_SDA, I2C_SCL);
 	lcd.backlight();
   lcd.setCursor(1, 0);
@@ -46,16 +56,15 @@ void setup(){
 }
 
 void loop(){
+//definimos las variables para medir el cambio de voltaje en los potenciómetros y analizar si hubo un cambio con los valores previos
   float volt1V, volt2V;
+  int potC = 0;
   volt1V = volt1;
   volt2V = volt2;
+//Leemos el voltaje de los potenciómetros y los guardamos en las variables indicadas
   volt1 = analogReadMilliVolts(Pot1)/10.0;
   volt2 = analogReadMilliVolts(Pot2)/10.0;
-
-  Serial.println("Voltajes");
-  Serial.println(volt1);
-  Serial.println(volt2);
-
+//separamos los voltajes en los diferentes dígitos para su impresión
   dig1[0] = volt1/100;
   dig1[1] = ((int)volt1%100)/10;
   dig1[2] = ((int)volt1%10);
@@ -63,11 +72,11 @@ void loop(){
   dig2[0] = volt2/100;
   dig2[1] = ((int)volt2%100)/10;
   dig2[2] = ((int)volt2%10);
-  
+  //establecemos la lógica para detectar cambios de voltaje y activar la variable envio 
   if(volt1 != volt1V | volt2 != volt2V){
-    envio = 1;
+    potC = 1;
   }
-
+//al activarse la variable envio se revisa si el valor de contador debe de aumentar y cambia los valores de esto
   if(envio == 1){
     if(cambio == 1 & contador != 255){
       contador++;
@@ -78,7 +87,8 @@ void loop(){
     Serial.println("Contador");
     Serial.println(contador);
   }
-  if(envio == 1){
+//envia los datos a la LCD dependiendo si hubo un cambio en el voltaje detectado o en el contador
+  if(envio == 1 | potC = 1){
     lcd.clear();
     lcd.backlight();
     lcd.setCursor(1,0);
@@ -113,10 +123,11 @@ void loop(){
     lcd.setCursor(13, 1);
     lcd.print(contador);
     envio = 0;
+	potC = 0;
   }
   delay(300);
 }
-
+//Interrupciones se activan con los botones respectivos y cambian las variables envio y cambio que se encargan de sumar o restar valores al contador y activar el envio a la I2C
 void IRAM_ATTR suma(void){
   unsigned long currentTime = millis();
   if ((currentTime - debounce1) > 100) {
